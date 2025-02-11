ec2fcd7cd55d1a39f1e8e2e04539d332bc3c2d03
Muhammad-Hashir-Waheed <79890092+Muhammad-Hashir-Waheed@users.noreply.github.com>
2025-02-12 02:02:28 +0500

    First Commit
    Uploaded the existing Modules

---
+++ src/Content.js
@@ -0,0 +1,256 @@
+// import images
+import Hero_person from "./assets/images/Hero/person.png";
+
+import figma from "./assets/images/Skills/figma.png";
+import sketch from "./assets/images/Skills/sketch.png";
+import ps from "./assets/images/Skills/ps.png";
+import reactjs from "./assets/images/Skills/react.png";
+import nodejs from "./assets/images/Skills/node.png";
+import python from "./assets/images/Skills/python.png";
+import Tailwind from "./assets/images/Skills/tailwindcss.png"
+import TypeScript from "./assets/images/Skills/typescript.png"
+import Nextjs from "./assets/images/Skills/nextjs.png"
+import Javascript from "./assets/images/Skills/javascript.png"
+import css3 from "./assets/images/Skills/css3.png"
+import HTML5 from "./assets/images/Skills/html5.png"
+import Bootstrap5 from "./assets/images/Skills/bootstrap.svg"
+import GitHub from "./assets/images/Skills/github.png"
+import MaterialUI from "./assets/images/Skills/materialUI.png"
+import Vercel from "./assets/images/Skills/vercel.png"
+import AntDesign from "./assets/images/Skills/ant-design.png"
+
+import services_logo1 from "./assets/images/Services/logo1.png";
+import services_logo2 from "./assets/images/Services/logo2.png";
+import services_logo3 from "./assets/images/Services/logo3.png";
+
+import project1 from "./assets/images/projects/img1.png";
+import project2 from "./assets/images/projects/img2.png";
+import project3 from "./assets/images/projects/img3.png";
+import person_project from "./assets/images/projects/person.png";
+
+import avatar1 from "./assets/images/Testimonials/avatar1.png";
+import avatar2 from "./assets/images/Testimonials/avatar2.png";
+import avatar3 from "./assets/images/Testimonials/avatar3.png";
+import avatar4 from "./assets/images/Testimonials/avatar4.png";
+
+import Hireme_person from "./assets/images/Hireme/person.png";
+import Hireme_person2 from "./assets/images/Hireme/person2.png";
+
+// import icons from react-icons
+import { GrMail } from "react-icons/gr";
+import { MdArrowForward, MdCall } from "react-icons/md";
+import { BsInstagram } from "react-icons/bs";
+import { TbSmartHome } from "react-icons/tb";
+import { BiUser } from "react-icons/bi";
+import { RiServiceLine, RiProjectorLine } from "react-icons/ri";
+import { MdOutlinePermContactCalendar } from "react-icons/md";
+
+export const content = {
+  nav: [
+    {
+      link: "#home",
+      icon: TbSmartHome,
+    },
+    {
+      link: "#skills",
+      icon: BiUser,
+    },
+    {
+      link: "#services",
+      icon: RiServiceLine,
+    },
+    {
+      link: "#projects",
+      icon: RiProjectorLine,
+    },
+    {
+      link: "#contact",
+      icon: MdOutlinePermContactCalendar,
+    },
+  ],
+  hero: {
+    title: "Web Developer",
+    firstName: "Muhammad Hashir",
+    LastName: "Waheed",
+    btnText: "Hire Me",
+    image: Hero_person,
+    hero_content: [
+      {
+        count: "3+",
+        text: "Years of Experinse in Web development",
+      },
+      {
+        count: "10+",
+        text: "Projects Worked in my career",
+      },
+    ],
+  },
+  skills: {
+    title: "Skills",
+    subtitle: "MY TOP SKILLS",
+    skills_content: [
+      {
+        name: "Javascript(ES6+)",
+        // para: "Lorem ipsum text  dummy",
+        logo: Javascript,
+      },
+      {
+        name: "React js",
+        // para: "Lorem ipsum text  dummy",
+        logo: reactjs,
+      },
+      {
+        name: "Next js",
+        // para: "Lorem ipsum text  dummy",
+        logo:  Nextjs,
+      },
+      {
+        name: "TypeScript",
+        // para: "Lorem ipsum text  dummy",
+        logo: TypeScript,
+      },
+      {
+        name: "HTML5",
+        // para: "Lorem ipsum text  dummy",
+        logo: HTML5,
+      },
+      {
+        name: "Tailwind CSS",
+        // para: "Lorem ipsum text  dummy",
+        logo: Tailwind,
+      },
+      {
+        name: "CSS3",
+        // para: "Lorem ipsum text  dummy",
+        logo: css3,
+      },
+      {
+        name: "Bootstrap5",
+        // para: "Lorem ipsum text  dummy",
+        logo: Bootstrap5,
+      },
+      {
+        name: "Github",
+        // para: "Lorem ipsum text  dummy",
+        logo: GitHub,
+      }, {
+        name: "Material UI",
+        // para: "Lorem ipsum text  dummy",
+        logo: MaterialUI,
+      },
+      {
+        name: "Ant-Design",
+        // para: "Lorem ipsum text  dummy",
+        logo: AntDesign,
+      },
+      {
+        name: "Vercel",
+        // para: "Lorem ipsum text  dummy",
+        logo: Vercel,
+      },
+    ],
+    icon: MdArrowForward,
+  },
+  services: {
+    title: "Services",
+    subtitle: "WHAT I OFFER",
+    service_content: [
+      {
+        title: "Web Development",
+        para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+        logo: services_logo1,
+      },
+      {
+        title: "ui / ux DESIGNING",
+        para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+        logo: services_logo2,
+      },
+      {
+        title: "PhotoShop Editing",
+        para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+        logo: services_logo3,
+      },
+    ],
+  },
+  Projects: {
+    title: "Projects",
+    subtitle: "MY CREATION",
+    image: person_project,
+    project_content: [
+      {
+        title: "Gym Website",
+        image: project1,
+      },
+      {
+        title: "Social Media web",
+        image: project2,
+      },
+      {
+        title: "Creative Website",
+        image: project3,
+      },
+    ],
+  },
+  Testimonials: {
+    title: "Testimonials",
+    subtitle: "MY CLIENT REVIEWS",
+    testimonials_content: [
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar1,
+        name: "JOHN DOE",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar2,
+        name: "Tom Alex",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar3,
+        name: "Johnny",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar4,
+        name: "ROBBIN",
+      },
+    ],
+  },
+  Hireme: {
+    title: "Hire Me",
+    subtitle: "FOR YOUR PROJECTS",
+    image1: Hireme_person,
+    image2: Hireme_person2,
+    para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+    btnText: "Hire Me",
+  },
+  Contact: {
+    title: "Contect Me",
+    subtitle: "GET IN TOUCH",
+    social_media: [
+      {
+        text: "hashirwaheed07@gmail.com",
+        icon: GrMail,
+        link: "mailto:codeaprogram@gmail.com",
+      },
+      {
+        text: "+92 337 0741355",
+        icon: MdCall,
+        link: "https://wa.me/1234567890",
+      },
+      {
+        text: "codeaprogram",
+        icon: BsInstagram,
+        link: "https://www.instagram.com/codeaprogram/",
+      },
+    ],
+  },
+  Footer: {
+    text: "All © Copy Right Reserved 2022",
+  },
+};
